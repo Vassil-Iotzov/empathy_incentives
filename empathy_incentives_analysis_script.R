9acf215697587ac7eb972700eeaa61a80ca34a32
Vassil-Iotzov <57714247+Vassil-Iotzov@users.noreply.github.com>
2021-08-06 14:50:18 +0200

    Add files via upload

--- empathy_incentives_analysis_script.R
+++ empathy_incentives_analysis_script.R
@@ -9,6 +9,7 @@ require(MuMIn)
 require(lme4)
 require(car)
 require(tidyverse)
+require(ggeffects)
 
 #set working directory
 setwd("...")
@@ -33,7 +34,11 @@ shapiro.test(empathy_incentives_df$v)
 shapiro.test(empathy_incentives_df$a)
 shapiro.test(empathy_incentives_df$z)
 ###################################################
-
+#The impression ratings were comparable between confederates
+model.impression<-lmer(impression ~ condition + (1|id), data = empathy_incentives_df)
+summary(model.impression)
+Anova(model.impression)
+##################################################
 #Empathy was induced with comparable strength in both conditions
 model.ind_ratio<-lmer(empathy_ratings_ln_scaled ~ condition + (1|id), data = empathy_incentives_df)
 summary(model.ind_ratio)
@@ -46,7 +51,37 @@ cor.test(empathy_incentives_df$empathy_ratings_ln[1:31],empathy_incentives_df$ec
 #The individual empathy ratings did not correlate with the personal distress (PD) scale
 cor.test(empathy_incentives_df$empathy_ratings_ln[1:31],empathy_incentives_df$pd[1:31])
 
-#no no significant difference in RTs
+#EC&PD Regression
+model.ec_pd<-lm(scale(empathy_ratings_ln[1:31]) ~ scale(ec[1:31]) + scale(pd[1:31]), data = empathy_incentives_df)
+summary(model.ec_pd)
+
+#The financial incentive increased the frequency of prosocial decisions
+model.prosoc_decisions<-lmer(prosocial_decisions_scaled ~ condition + (1|id), data = empathy_incentives_df)
+summary(model.prosoc_decisions)
+Anova(model.prosoc_decisions)
+r.squaredGLMM(model.prosoc_decisions)
+
+#Significantly more prosocial decisions in the empathy-alone condition compared to the baseline condition from Hein, G., Morishima, Y., Leiberg, S., Sul, S. & Fehr, E. (2016) 'The brain's functional network architecture reveals human motives', Science, 80(351), pp. 1074-1078.
+mean(empathy_incentives_df$prosocial_decisions_percent[empathy_incentives_df$condition==1])
+sd(empathy_incentives_df$prosocial_decisions_percent[empathy_incentives_df$condition==1])/sqrt(31)
+mean(baseline_data$percentage)
+sd(baseline_data$percentage)/sqrt(34)
+t.test(empathy_incentives_df$prosocial_decisions_percent[empathy_incentives_df$condition==1],baseline_data$percentage)
+
+#not included: Percent change in prosocial decisions in the empathy-bonus condition relative to the empathy-alone condition
+#t.test(empathy_incentives_df$prosocial_decisions_percent_ratio[1:31], mu = 0, alternative = "two.sided")
+
+#empathy ratings related to the probability of prosocial decisions
+model.prosoc_decis<-glmer(response ~ condition * empathy_ratings + (1 | subj_idx), data = trial_by_trial, family = binomial)
+summary(model.prosoc_decis)
+Anova(model.prosoc_decis)
+r.squaredGLMM(model.prosoc_decis)
+
+#The lower an individual's empathy ratings, the stronger the increase in the frequency of prosocial decisions in the empathy-bonus condition relative to the empathy-alone condition 
+model.prosoc_decision_dif<-lm(scale(prosocial_decisions_percent[32:62]-prosocial_decisions_percent[1:31]) ~ scale(empathy_ratings_empathy_cond_ln[1:31]) , data = empathy_incentives_df)
+summary(model.prosoc_decision_dif)
+
+#no significant difference in RTs
 #prosocial decisions:
 model.rts<-lmer(rt_scaled ~ condition + (1|id), data = empathy_incentives_df)
 summary(model.rts)
@@ -62,71 +97,17 @@ model.rts<-lmer(rt_all_scaled ~ condition + (1|id), data = empathy_incentives_df
 summary(model.rts)
 Anova(model.rts)
 
-#The financial incentive increased the frequency of prosocial decisions
-model.prosoc_decisions<-lmer(prosocial_decisions_scaled ~ condition + (1|id), data = empathy_incentives_df)
-summary(model.prosoc_decisions)
-Anova(model.prosoc_decisions)
-r.squaredGLMM(model.prosoc_decisions)
-
-#Percent change in prosocial decisions in the empathy-bonus condition relative to the empathy-alone condition
-t.test(empathy_incentives_df$prosocial_decisions_percent_ratio[1:31], mu = 0, alternative = "two.sided")
-
-#Significantly more prosocial decisions in the empathy-alone condition compared to the baseline condition from Hein, G., Morishima, Y., Leiberg, S., Sul, S. & Fehr, E. (2016) 'The brain's functional network architecture reveals human motives', Science, 80(351), pp. 1074-1078.
-mean(empathy_incentives_df$prosocial_decisions_percent[empathy_incentives_df$condition==1])
-sd(empathy_incentives_df$prosocial_decisions_percent[empathy_incentives_df$condition==1])/sqrt(31)
-mean(baseline_data$percentage)
-sd(baseline_data$percentage)/sqrt(34)
-t.test(empathy_incentives_df$prosocial_decisions_percent[empathy_incentives_df$condition==1],baseline_data$percentage)
-
 #Higher empathy ratings predicted faster prosocial decision
 model.rt_empathy<-lmer(rt_scaled ~ condition*scale(empathy_ratings_empathy_cond_ln) + (1|id), data = empathy_incentives_df)
 summary(model.rt_empathy)
 Anova(model.rt_empathy)
 r.squaredGLMM(model.rt_empathy)
 
-#The lower an individual's empathy ratings, the stronger the increase in the frequency of prosocial decisions in the empathy-bonus condition relative to the empathy-alone condition
-model.prosoc_decision_ratio<-lm(scale(prosocial_decisions_percent_ratio[1:31]) ~ scale(empathy_ratings_empathy_cond_ln[1:31]) , data = empathy_incentives_df)
-summary(model.prosoc_decision_ratio)
+##### For HDDM Results please see HDDM Files ##################
 
-#The financial incentive increased the speed of information accumulation, but not the initial decision preference
-#v
-mean(empathy_incentives_df$v[empathy_incentives_df$condition==0])
-sd(empathy_incentives_df$v[empathy_incentives_df$condition==0])/sqrt(31)
-mean(empathy_incentives_df$v[empathy_incentives_df$condition==1])
-sd(empathy_incentives_df$v[empathy_incentives_df$condition==1])/sqrt(31)
-
-#z
-mean(empathy_incentives_df$z[empathy_incentives_df$condition==0])
-sd(empathy_incentives_df$z[empathy_incentives_df$condition==0])/sqrt(31)
-mean(empathy_incentives_df$z[empathy_incentives_df$condition==1])
-sd(empathy_incentives_df$z[empathy_incentives_df$condition==1])/sqrt(31)
-
-#a
-mean(empathy_incentives_df$a[empathy_incentives_df$condition==0])
-sd(empathy_incentives_df$a[empathy_incentives_df$condition==0])/sqrt(31)
-mean(empathy_incentives_df$a[empathy_incentives_df$condition==1])
-sd(empathy_incentives_df$a[empathy_incentives_df$condition==1])/sqrt(31)
-
-
-#same analysis with HDDM Regression model
-mean(hddm_reg_df$hddm_reg_v[hddm_reg_df$condition==0])
-sd(hddm_reg_df$hddm_reg_v[hddm_reg_df$condition==0])/sqrt(31)
-mean(hddm_reg_df$hddm_reg_v[hddm_reg_df$condition==1])
-sd(hddm_reg_df$hddm_reg_v[hddm_reg_df$condition==1])/sqrt(31)
-
-mean(hddm_reg_df$hddm_reg_z[hddm_reg_df$condition==0])
-sd(hddm_reg_df$hddm_reg_z[hddm_reg_df$condition==0])/sqrt(31)
-mean(hddm_reg_df$hddm_reg_z[hddm_reg_df$condition==1])
-sd(hddm_reg_df$hddm_reg_z[hddm_reg_df$condition==1])/sqrt(31)
-
-mean(hddm_reg_df$hddm_reg_a[hddm_reg_df$condition==0])
-sd(hddm_reg_df$hddm_reg_a[hddm_reg_df$condition==0])/sqrt(31)
-mean(hddm_reg_df$hddm_reg_a[hddm_reg_df$condition==1])
-sd(hddm_reg_df$hddm_reg_a[hddm_reg_df$condition==1])/sqrt(31)
-
-#The anterior insula tracks the incentive-related facilitation of prosocial decisions and individual differences in empathy
-model.betas_mean<-lm(formula=ai_betas_average_average_sclaed[1:31] ~ empathy_ratings_ln_scaled[1:31],data = empathy_incentives_df)
-summary(model.betas_mean)
+#not included: The anterior insula tracks the incentive-related facilitation of prosocial decisions and individual differences in empathy
+#model.betas_mean<-lm(formula=ai_betas_average_average_sclaed[1:31] ~ empathy_ratings_ln_scaled[1:31],data = empathy_incentives_df)
+#summary(model.betas_mean)
 
 #The financial incentive has a differential effect on anterior insular activation in high and low empathic individuals
 model.betas_mean<-lmer(formula=ai_betas_average_scaled ~ empathy_ratings_ln_scaled*v_scaled*condition +  (1|id), data = empathy_incentives_df)
@@ -134,47 +115,42 @@ summary(model.betas_mean)
 Anova(model.betas_mean,   type = 3) #Type 3 because of sig. interaction effects.
 r.squaredGLMM(model.betas_mean)
 
-#same analysis with the beta estimates extracted from the pallidum, right lingual gyrus and left inferior lingual gyrus
-#pallidum:
-model.betas_pallidum<-lmer(formula=betas_pallidum_scaled ~ empathy_ratings_ln_scaled*v_scaled*condition +  (1|id), data = empathy_incentives_df)
-summary(model.betas_pallidum)
-Anova(model.betas_pallidum,   type = 3) #Type 3 because of sig. interaction effects.
-r.squaredGLMM(model.betas_pallidum)
-
+#same analysis with the beta estimates extracted from right lingual gyrus
 #right lingual gyrus
 model.betas_rlg<-lmer(formula=betas_right_lingual_scaled ~ empathy_ratings_ln_scaled*v_scaled*condition +  (1|id), data = empathy_incentives_df)
 summary(model.betas_rlg)
 Anova(model.betas_rlg,   type = 3) #Type 3 because of sig. interaction effects.
 r.squaredGLMM(model.betas_rlg)
 
-#left inferior lingual gyrus
-model.betas_llg<-lmer(formula=betas_left_lingual_scaled ~ empathy_ratings_ln_scaled*v_scaled*condition +  (1|id), data = empathy_incentives_df)
-summary(model.betas_llg)
-Anova(model.betas_llg,   type = 3) #Type 3 because of sig. interaction effects.
-r.squaredGLMM(model.betas_llg)
-
 #To unpack the significant condition x v-parameter x empathy rating interaction in AI, we tested the relationship between the v-parameter and the empathy ratings separately in the empathy-alone and the empathy-bonus condition
-model.betas_mean_emp<-lm(formula=ai_betas_average_scaled[1:31] ~ empathy_ratings_ln_scaled[1:31]*v_scaled[1:31], data = empathy_incentives_df)
-summary(model.betas_mean_emp)
-model.betas_mean_empego<-lm(formula=ai_betas_average_scaled[32:62] ~ empathy_ratings_ln_scaled[32:62]*v_scaled[32:62], data = empathy_incentives_df)
+empathy_incentives_empego_df<-empathy_incentives_df[32:62,]
+model.betas_mean_empego<-lm(formula=ai_betas_average_scaled ~ empathy_ratings_ln_scaled*v_scaled, data = empathy_incentives_empego_df)
 summary(model.betas_mean_empego)
-
-
-model.betas_mean_emp<-lm(formula=ai_betas_average_scaled[1:31] ~ v_scaled[1:31], data = empathy_incentives_df)
+empathy_incentives_emp_df<-empathy_incentives_df[1:31,]
+model.betas_mean_emp<-lm(formula=ai_betas_average_scaled ~ empathy_ratings_ln_scaled*v_scaled, data = empathy_incentives_emp_df)
 summary(model.betas_mean_emp)
 
+#significant positive relationship between empathy ratings and AI beta estimates (one-sided)
 model.betas_mean_emp<-lm(formula=ai_betas_average_scaled[1:31] ~ empathy_ratings_ln_scaled[1:31], data = empathy_incentives_df)
 summary(model.betas_mean_emp)
 
+#significant positive relationship between empathy ratings and drift rate (one-sided)
 model.v<-lm(formula=v_scaled[1:31] ~ empathy_ratings_ln_scaled[1:31], data = empathy_incentives_df)
 summary(model.v)
 
+#significant positive relationship between v-parameter and AI beta estimates (one-sided)
+model.betas_mean_emp<-lm(formula=ai_betas_average_scaled[1:31] ~ v_scaled[1:31], data = empathy_incentives_df)
+summary(model.betas_mean_emp)
+
+#significant positive relationship between v-parameter and AI beta estimates (one-sided)
 model.betas_mean_empego<-lm(formula=ai_betas_average_scaled[32:62] ~ v_scaled[32:62], data = empathy_incentives_df)
 summary(model.betas_mean_empego)
-
+ 
+#no significant relationship between empathy ratings and AI beta estimates (one-sided)
 model.betas_mean_empego<-lm(formula=ai_betas_average_scaled[32:62] ~ empathy_ratings_ln_scaled[32:62], data = empathy_incentives_df)
 summary(model.betas_mean_empego)
 
+#no significant relationship between empathy ratings and drift rate (one-sided)
 model.v<-lm(formula=v_scaled[32:62] ~ empathy_ratings_ln_scaled[32:62], data = empathy_incentives_df)
 summary(model.v)
 
